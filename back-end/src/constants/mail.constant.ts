fad8d932187d66ae548cc0cc41c413a1c4bdc16e
ThanhLD <ducthanh98a@gmail.com>
2019-11-20 18:41:47 +0700

    add mail server

---
+++ back-end/src/constants/mail.constant.ts
@@ -0,0 +1,10 @@
+export class MailTemplate {
+    public static EMAIL_CONFIRM_MSG_HEADER:
+        '<h2>Bạn nhận được email này vì đã đăng kí tài khoản tâm sự thầm kín tuổi 18+ </h2><br/><h3>Bạn vui lòng kích vào link bên dưới để kích hoạt tài khoản: ';
+    public static EMAIL_CONFIRM_MSG_FOOTER:
+        'Nếu bạn không đăng kí gì ở ứng dụng của chúng tôi. Vui lòng bỏ qua email này. Xin cảm ơn !!!';
+    public static EMAIL_SERVER_ERR:
+        'Hệ thống gửi mail đang quá tải. Bạn có thể đăng nhập để yêu cầu kích hoạt mail sau ít phút. Xin cảm ơn !!!';
+    public static SUBJECT:
+        'Hệ thống quản lý cho thuê nhà trọ sinh viên';
+}
---
+++ back-end/src/helpers/nodemailer.helper.ts
@@ -0,0 +1,53 @@
+// @flow
+import * as nodemailer from 'nodemailer';
+import { SendMailOptions } from 'nodemailer';
+import SMTPTransport = require('nodemailer/lib/smtp-transport');
+import Mail = require('nodemailer/lib/mailer');
+import { Logger } from '@nestjs/common';
+import { MailTemplate } from '../constants/mail.constant';
+import { from } from 'rxjs/internal/observable/from';
+import { Observable } from 'rxjs';
+import { bindCallback } from 'rxjs';
+
+export class NodeMailer {
+    private transporter: Mail;
+    constructor() {
+        const transportOptions: SMTPTransport.Options = {
+            host: process.env.MAIL_HOST,
+            port: +process.env.MAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        };
+        this.transporter = nodemailer.createTransport(transportOptions);
+        this.verifyTransporter();
+    }
+
+    private verifyTransporter(): void {
+        this.transporter.verify(err => {
+            if (err) {
+                return Logger.error(err.message, err.stack, 'Node Mailer');
+            }
+            return Logger.warn('The mail server has been started');
+        });
+    }
+
+    sendMail(to, subject, content): Observable<any> {
+        const options: SendMailOptions = {
+            from: process.env.MAIL_USER,
+            to,
+            subject,
+            html: this.createTemplate(content),
+        };
+        return from(this.transporter.sendMail(options));
+    }
+
+    private createTemplate(content): string {
+        return `${MailTemplate.EMAIL_CONFIRM_MSG_HEADER}<br/>
+            <h3><a href=${content} target="blank">${content}<a/></h3>
+            ${MailTemplate.EMAIL_CONFIRM_MSG_FOOTER}
+        `;
+    }
+}
